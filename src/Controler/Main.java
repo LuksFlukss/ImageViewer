e3c73a1a1b93c332f0487e5a0ea574d3c8dd4c52
LuksFlukss <loukavanhoucke480@gmail.com>
2023-01-19 09:31:14 +0000

    Versión 1

---
+++ src/Controler/Main.java
@@ -0,0 +1,20 @@
+package Controler;
+
+import java.io.File;
+import Model.Image;
+import Persistence.FileImageLoader;
+
+/**
+ * 
+ * @author Louka Vanhoucke
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        File file = new File("images");
+        FileImageLoader imageLoader = new FileImageLoader(file);
+        Image image = imageLoader.load();
+        MainFrame mainFrame = new MainFrame(imageLoader);
+        mainFrame.getImageDisplay().show(image);
+    }
+}
